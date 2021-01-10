70b11a502f4c07ee4cde1c7d46dd8a1251bc451b
Abdullah ALmeshekah <meshekah@MacBook-Pro.local>
2021-01-10 13:12:19 +0300

    solved

---
+++ story.js
@@ -0,0 +1,6 @@
+const name = 'Mshary' ;
+const age =25 ;
+const hobby ='Meditation'; 
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
+
